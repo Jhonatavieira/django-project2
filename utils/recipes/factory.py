553e22878d2553f5a100e2b50162a3f7e09b71ba
Jhonata Vieira de Andrade <jhonatavieira@yahoo.com.br>
2024-10-13 18:52:36 -0300

    Inserting details through the module factory

--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -1,33 +1,32 @@
+# from inspect import signature
 from random import randint
 from faker import Faker
-from pprint import pprint
+
+fake = Faker('pt_BR')
 
 
 def rand_ratio():
     return randint(840, 900), randint(473, 573)
 
 
-fake = Faker('pt-BR')
-
-
 def make_recipe():
     return {
+        'id': fake.random_number(digits=2, fix_len=True),
         'title': fake.sentence(nb_words=6),
         'description': fake.sentence(nb_words=12),
         'preparation_time': fake.random_number(digits=2, fix_len=True),
-        'preparation_time_unit': 'Minute',
+        'preparation_time_unit': 'Minutos',
         'servings': fake.random_number(digits=2, fix_len=True),
         'servings_unit': 'Porção',
         'preparation_steps': fake.text(3000),
         'created_at': fake.date_time(),
-        'autor': {
+        'author': {
             'first_name': fake.first_name(),
             'last_name': fake.last_name(),
         },
         'category': {
             'name': fake.word()
         },
-
         'cover': {
             'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
         }
@@ -35,4 +34,5 @@ def make_recipe():
 
 
 if __name__ == '__main__':
+    from pprint import pprint
     pprint(make_recipe())
